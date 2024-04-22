b574d8a4835119e7c789083c59ccdfb26848a915
ojiputra <ojishefals2@gmail.com>
2024-04-22 09:48:22 +0700

    menambah card eror

--- resources/views/dashboard/layanan/layanan.blade.php
+++ resources/views/dashboard/layanan/layanan.blade.php
@@ -18,12 +18,16 @@
                 </div>
             </div>
         @empty
-            <div class="error-page container">
-                <div class="col-md-8 col-12 offset-md-2">
-                    <div class="text-center">
-                        <img class="img-error" src="icon/error-404.svg" alt="Not Found">
-                        <h1 class="error-title">NOT FOUND</h1>
-                        <p class='fs-5 text-gray-600'>The page you are looking not found.</p>
+            <div class="card">
+                <div class="card-body">
+                    <div class="error-page container">
+                        <div class="col-md-8 col-12 offset-md-2">
+                            <div class="text-center">
+                                <img class="img-error" src="icon/error-404.svg" alt="Not Found">
+                                <h1 class="error-title">NOT FOUND</h1>
+                                <p class='fs-5 text-gray-600'>The page you are looking not found.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
